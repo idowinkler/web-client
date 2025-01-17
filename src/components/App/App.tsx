300564821f8b73a2402c589028135cd60e2d1641
Lior Rachmani <liorachmani@gmail.com>
2025-01-17 18:46:13 +0200

    add content component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Style from "./App.module.css";
 import { SelectedUserContextProvider } from "../SelectedUserContext/SelectedUserContext";
 import SideBar from "../SideBar/SideBar";
+import Content from "../Content/Content";
 
 export const App: React.FC = ({}) => {
   return (
     <SelectedUserContextProvider>
       <div className={Style.app}>
         <SideBar />
+        <Content />
       </div>
     </SelectedUserContextProvider>
   );
