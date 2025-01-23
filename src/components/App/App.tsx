e0bfa683117e6b9a901b183e2256c0e00f267810
Lior Rachmani <liorachmani@gmail.com>
2025-01-23 20:08:37 +0200

    routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Style from "./App.module.css";
 import { SelectedUserContextProvider } from "../SelectedUserContext/SelectedUserContext";
-import SideBar from "../SideBar/SideBar";
 import Content from "../Content/Content";
+import Register from "../../pages/Register";
+import { AuthProvider } from "../AuthContext";
+import Login from "../../pages/Login";
+import ProtectedRoute from "../ProtectedRoute";
 
 export const App: React.FC = ({}) => {
   return (
-    <SelectedUserContextProvider>
-      <div className={Style.app}>
-        <SideBar />
-        <Content />
-      </div>
-    </SelectedUserContextProvider>
+    <AuthProvider>
+      <SelectedUserContextProvider>
+        <div className={Style.app}>
+          <Routes>
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Content />} />
+              <Route path="*" element={<Content />} />
+            </Route>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </div>
+      </SelectedUserContextProvider>
+    </AuthProvider>
   );
 };
 
